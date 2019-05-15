462b4936d0a6bc08e0a7829a6c76fc1ea7eb64bd
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-15 19:26:02 +0000

    Automatically backed up by Learn

--- spec/object_initialize_spec.rb
+++ spec/object_initialize_spec.rb
@@ -21,12 +21,12 @@ it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
       expect(fido.instance_variable_get(:@breed)).to eq("Mutt")
     end
   end
-end
+
 
     it 'sets the breed of the dog in an instance variable @breed' do
       fido = Dog.new("Fido", "Pug")
 
       expect(fido.instance_variable_get(:@breed)).to eq("Pug")
     end
-
+end
     
